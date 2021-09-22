444d02cf877feed859c5e4c600893224f1f29e2d
unknown <dtulpa16@yahoo.com>
2021-09-22 16:06:26 -0500

    game now ends when either player reaches two wins

--- RPSLS/Fight.cs
+++ RPSLS/Fight.cs
@@ -12,35 +12,147 @@ namespace RPSLS
         {
             Human playerOne = new Human(0);
             Human playerTwo = new Human(0);
-            string playerOneChoice = playerOne.GetChoice();
-            string playerTwoChoice = playerTwo.GetChoice();
-            if (playerOneChoice == "rock")
+            while(playerOne.numberOfWins < 3 && playerTwo.numberOfWins < 3) 
             {
-                if (playerTwoChoice == "rock")
+                Console.WriteLine(playerOne.numberOfWins);
+                string playerOneChoice = playerOne.GetChoice();
+                string playerTwoChoice = playerTwo.GetChoice();
+                if (playerOneChoice == "rock")
                 {
-                    Console.WriteLine("Rock ties with rock!");
+                    if (playerTwoChoice == "rock")
+                    {
+                        Console.WriteLine("Rock ties with rock!");
+                    }
+                    else if (playerTwoChoice == "paper")
+                    {
+                        Console.WriteLine("Paper covers rock! Player two wins");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "scissors")
+                    {
+                        Console.WriteLine("Rock smashes scissors! Player one wins");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "lizard")
+                    {
+                        Console.WriteLine("Rock smashes lizard! Player one wins");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "spock")
+                    {
+                        Console.WriteLine("Spock vaporizes rock! Player two wins");
+                        playerTwo.numberOfWins += 1;
+                    }
                 }
-                else if (playerTwoChoice == "paper")
+                else if(playerOneChoice == "paper")
                 {
-                    Console.WriteLine("Paper covers rock! Player two wins");
-                    playerTwo.numberOfWins += 1;
-                    Console.WriteLine(playerTwo.numberOfWins);
-
+                    if (playerTwoChoice == "rock")
+                    {
+                        Console.WriteLine("Paper covers rock! Player one wins");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "paper")
+                    {
+                        Console.WriteLine("Paper ties with paper!");
+                    }
+                    else if (playerTwoChoice == "scissors")
+                    {
+                        Console.WriteLine("scissors cuts paper! player two wins");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "lizard")
+                    {
+                        Console.WriteLine("Lizard eats paper! Player two wins");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "spock")
+                    {
+                        Console.WriteLine("Paper disproves spock! player one wins");
+                        playerOne.numberOfWins += 1;
+                    }
                 }
-                else if (playerTwoChoice == "scissors")
+                else if (playerOneChoice == "scissors")
                 {
-                    Console.WriteLine("Rock smashes scissors! Player one wins");
+                    if (playerTwoChoice == "rock")
+                    {
+                        Console.WriteLine("Rock crushed paper! Player two wins");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "paper")
+                    {
+                        Console.WriteLine("Scissors cuts paper! Player one wins");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "scissors")
+                    {
+                        Console.WriteLine("Scissors ties with scissors!");
+                    }
+                    else if (playerTwoChoice == "lizard")
+                    {
+                        Console.WriteLine("scissors decapitates lizard, player 1 wins!");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "spock")
+                    {
+                        Console.WriteLine("spock smashes scissors, player 2 wins.");
+                        playerTwo.numberOfWins += 1;
+                    }
                 }
-                else if (playerTwoChoice == "lizard")
+                else if (playerOneChoice == "lizard")
                 {
-                    Console.WriteLine("Rock smashes lizard! Player one wins");
+                    if (playerTwoChoice == "rock")
+                    {
+                        Console.WriteLine("Rock crushed lizard! Player two wins");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "paper")
+                    {
+                        Console.WriteLine("Lizard eats paper! Player one wins");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "scissors")
+                    {
+                        Console.WriteLine("scissors decapitates lizard, player 2 wins.");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "lizard")
+                    {
+                        Console.WriteLine("Lizard ties lizard!");
+                    }
+                    else if (playerTwoChoice == "spock")
+                    {
+                        Console.WriteLine("lizard poisons spock, player 1 wins!");
+                        playerOne.numberOfWins += 1;
+                    }
                 }
-                else if (playerTwoChoice == "spock")
+                else if (playerOneChoice == "spock")
                 {
-                    Console.WriteLine("Spock vaporizes rock! Player two wins");
+                    if (playerTwoChoice == "rock")
+                    {
+                        Console.WriteLine("spock vaporizes rock, player 2 wins.");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "paper")
+                    {
+                        Console.WriteLine("paper disproves spock, player 2 wins.");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "scissors")
+                    {
+                        Console.WriteLine("spock smashes scissors, player 1 wins!");
+                        playerOne.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "lizard")
+                    {
+                        Console.WriteLine("lizard poisons spock, player 2 wins.");
+                        playerTwo.numberOfWins += 1;
+                    }
+                    else if (playerTwoChoice == "spock")
+                    {
+                        Console.WriteLine("Spock ties spock!");
+                    }
                 }
             }
-
         }
     }
 }
